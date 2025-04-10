715e8922b8007b33c8197a6d2e009eb3fc3638ac
Maxwell Gibbard <134660926+maxgibbardd@users.noreply.github.com>
2025-04-10 10:22:55 -0400

    landing page height fixed

--- components/LandingPage/Hero.js
+++ components/LandingPage/Hero.js
@@ -20,7 +20,7 @@ const Hero = () => {
 const Section = styled.section`
   background: linear-gradient(135deg, navy, darkgreen);
   width: 100%;
-  height: 60vh;
+  height: 86.3vh;
   display: flex;
   align-items: center;
   justify-content: center;
